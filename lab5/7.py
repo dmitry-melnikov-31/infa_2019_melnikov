18a5139057d8eed06b4302b90fbc07120f55d9d5
Dmitry_Melnikov <dmitry.melnikov@vicuesoft.com>
2019-10-03 16:45:49 +0300

    oni letayut

---
+++ lab5/7.py
@@ -0,0 +1,69 @@
+from graph import*
+
+penColor(150,150,150) #фон
+brushColor(150,150,150)
+rectangle(0,0,400,600)
+
+brushColor(255,255,255) 
+penColor(150,150,150)
+circle(350,60,40)
+
+brushColor(0,0,0) #фон
+penColor(0,0,0)
+rectangle(0,280,400,600)
+
+brushColor(80,67,0) #дом
+penColor(80,67,0)
+rectangle(20,130,230,460)
+
+brushColor(43,17,0) #окна низ
+penColor(43,17,0)
+rectangle(44,425,82,370)
+
+brushColor(43,17,0) 
+penColor(43,17,0)
+rectangle(106,425,144,370)
+
+brushColor("yellow")
+penColor("yellow")
+rectangle(168,425,206,370)
+
+brushColor(72,62,55) #окна верх
+penColor(72,62,55)
+rectangle(35,275,58,130)
+rectangle(80,275,103,130)
+rectangle(133,275,156,130)
+rectangle(186,275,209,130)
+
+brushColor(26,26,26) #балкон
+penColor(26,26,26)
+rectangle(0,280,250,310)
+rectangle(13,260,237,247)
+rectangle(5,280,13,260)
+rectangle(245,280,237,260)
+rectangle(30,280,46,260)
+rectangle(71,280,87,260)
+rectangle(112,280,128,260)
+rectangle(153,280,169,260)
+rectangle(194,280,210,260)
+
+brushColor(0,0,0) #крыша
+penColor(0,0,0)
+polygon([(0,130),(250,130),(217,107),(33,107)])
+
+brushColor(26,26,26) #трубы на крыше
+penColor(26,26,26)
+rectangle(51,120,58,85)
+rectangle(63,122,77,65)
+rectangle(156,107,162,90)
+rectangle(202,122,209,77)
+
+
+
+
+
+
+
+
+run()
+
